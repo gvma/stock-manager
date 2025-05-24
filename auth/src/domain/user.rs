6149e4221652c454990024c855f1419b6c2f3ca9
Gabriel Freire <fabriond@gmail.com>
2025-05-24 15:11:08 +0200

    moved logic to convert user to sign_in_output to the user model

--- auth/src/domain/user.rs
+++ auth/src/domain/user.rs
@@ -1,5 +1,10 @@
-// src/domain/models.rs
+use std::env;
+
+use chrono::{Duration, Utc};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use serde::{Serialize, Deserialize};
+
+use super::claims::Claims;
 
 #[derive(Serialize, Deserialize, sqlx::FromRow, Default)]
 pub struct User {
@@ -8,6 +13,23 @@ pub struct User {
 
     #[serde(skip_serializing)]
     pub password: String
+}
+
+impl User {
+    pub fn to_sign_in_output(&self) -> SignInOutput {
+        SignInOutput {
+            email: self.email.clone(),
+            name: self.name.clone(),
+            token: self.auth_token()
+        }
+    }
+    
+    pub fn auth_token(&self) -> String {
+        let claims = Claims { sub: self.email.to_owned(), exp: (Utc::now() + Duration::hours(24)).timestamp() as usize };
+        let secret = env::var("TOKEN_ENCRYPT_SECRET").expect("TOKEN_ENCRYPT_SECRET não definido");
+
+        encode(&Header::default(), &claims, &EncodingKey::from_secret(secret.as_ref())).unwrap()
+    }
 }
 
 #[derive(Serialize, Deserialize, Default)]
--- auth/src/handlers/user_handler.rs
+++ auth/src/handlers/user_handler.rs
@@ -1,26 +1,14 @@
 use axum::{Extension, Json, http::StatusCode};
 use sqlx::PgPool;
 use bcrypt::{DEFAULT_COST, hash, verify};
-use chrono::{Utc, Duration};
-use jsonwebtoken::{encode, EncodingKey, Header};
-use std::env;
 use crate::repository::user_repository::UserRepository;
 use crate::domain::user::{User, SignInInput, SignInOutput};
-use crate::domain::claims::Claims;
 
 pub async fn sign_in(Extension(pool): Extension<PgPool>, Json(user): Json<SignInInput>) -> Result<Json<SignInOutput>, (StatusCode, String)> {
   match UserRepository::get_by_email(&pool, user.email).await {
     Ok(found_user) => {
       if verify(user.password, &found_user.password).unwrap() {
-        let my_claims = Claims { sub: found_user.email.to_owned(), exp: (Utc::now() + Duration::hours(24)).timestamp() as usize };
-        let secret = env::var("TOKEN_ENCRYPT_SECRET").expect("TOKEN_ENCRYPT_SECRET não definido");
-        let token = encode(&Header::default(), &my_claims, &EncodingKey::from_secret(secret.as_ref())).unwrap();
-        let response = SignInOutput{
-          email: found_user.email,
-          name: found_user.name,
-          token
-        };
-        Ok(Json(response))
+        Ok(Json(found_user.to_sign_in_output()))
       } else {
         Err((StatusCode::NOT_FOUND, "Usuário não encontrado".to_string()))
       }
