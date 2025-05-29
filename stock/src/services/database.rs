7d711c2fff949b7af7643a2c20b1d67a76257338
Gabriel Freire <fabriond@gmail.com>
2025-05-29 05:17:09 +0200

    re-added database name to DATABASE_URL env variable

--- stock/src/services/database.rs
+++ stock/src/services/database.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use sqlx::{postgres::{PgConnectOptions, PgPoolOptions}, ConnectOptions, PgPool};
+use sqlx::{postgres::{PgPoolOptions}, PgPool};
 
 use super::Connect;
 
@@ -10,14 +10,9 @@ impl Connect<PgPool> for Database {
     async fn connect() -> PgPool {
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL não definido");
     
-        let connection_options =
-            PgConnectOptions::from_url(&database_url.parse().expect("DATABASE_URL inválido"))
-                .expect("DATABASE_URL inválido")
-                .database("stock-manager-stock");
-    
         PgPoolOptions::new()
             .max_connections(5)
-            .connect_with(connection_options)
+            .connect(&database_url)
             .await
             .expect("Erro ao conectar no banco")
     }
